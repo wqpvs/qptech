3a42c7d15819e01ffca7388ce77457fe5feb16cf
Quentin Preik <quentin@preik.net>
2021-08-11 08:01:39 -0400

    Added Small Water Tower, including language entry, and recipe

--- mods-src/qptech/src/misc/BEWaterTower.cs
+++ mods-src/qptech/src/misc/BEWaterTower.cs
@@ -21,33 +21,67 @@ namespace qptech.src
 {
     class BEWaterTower:BlockEntity
     {
-        int tick = 5000;
+        int tick = 7*5000;
         int waterPerTick = 1;
+        int bonusRainWaterPerTick = 1; //this is in liters, not a multiplier
         public override void Initialize(ICoreAPI api)
         {
             base.Initialize(api);
+            tick = Block.Attributes["tick"].AsInt(tick);
+            waterPerTick = Block.Attributes["waterPerTick"].AsInt(waterPerTick);
+            bonusRainWaterPerTick = Block.Attributes["bonusRainWaterPerTick"].AsInt(bonusRainWaterPerTick);
             RegisterGameTickListener(OnTick, tick);
         }
 
         public void OnTick(float tf)
         {
+            //Make sure there is a valid container
             BlockPos bp = Pos.Copy().Offset(BlockFacing.DOWN);
             BlockEntity checkblock = Api.World.BlockAccessor.GetBlockEntity(bp);
             if (checkblock == null) { return; }
             var checkcontainer = checkblock as BlockEntityContainer;
             if (checkcontainer == null) { return; }
-            
+            //Make sure block is exposed to sky
+            int worldheight = Api.World.BlockAccessor.GetRainMapHeightAt(Pos);
+            if (worldheight > Pos.Y) { return; }
+
+            //Setup some water to insert
+            int waterqty = waterPerTick;
+            int rainnear = Api.World.BlockAccessor.GetDistanceToRainFall(Pos);
+            if (rainnear < 99) { 
+                waterqty+=bonusRainWaterPerTick;
+            }
             Item outputItem = Api.World.GetItem(new AssetLocation("game:waterportion"));
-            ItemStack itmstk = new ItemStack(outputItem, waterPerTick);
+            ItemStack itmstk = new ItemStack(outputItem, waterqty);
             DummyInventory dummy = new DummyInventory(Api);
             dummy[0].Itemstack = itmstk;
-            WeightedSlot tryoutput = checkcontainer.Inventory.GetBestSuitedSlot(dummy[0]);
-            if (tryoutput.slot == null) { return; }
-            
-            ItemStackMoveOperation op = new ItemStackMoveOperation(Api.World, EnumMouseButton.Left, 0, EnumMergePriority.DirectMerge, dummy[0].StackSize);
 
-            dummy[0].TryPutInto(tryoutput.slot, ref op);
-            tryoutput.slot.MarkDirty();
+            WeightedSlot tryoutput = checkcontainer.Inventory.GetBestSuitedSlot(dummy[0]);
+            if (tryoutput.slot != null) {
+                ItemStackMoveOperation op = new ItemStackMoveOperation(Api.World, EnumMouseButton.Left, 0, EnumMergePriority.DirectMerge, dummy[0].StackSize);
+
+                dummy[0].TryPutInto(tryoutput.slot, ref op);
+                tryoutput.slot.MarkDirty();
+                return;
+            }
+            
+            /*for (int c = 0; c < checkcontainer.Inventory.Count;c++)
+            {
+                ItemSlotLiquidOnly lo = checkcontainer.Inventory[c] as ItemSlotLiquidOnly;
+                if (lo == null) { continue; }
+                if (lo.CanHold(dummy[0]))
+                {
+                    ItemStackMoveOperation op = new ItemStackMoveOperation(Api.World, EnumMouseButton.Left, 0, EnumMergePriority.DirectMerge, dummy[0].StackSize);
+
+                    dummy[0].TryPutInto(tryoutput.slot, ref op);
+                    tryoutput.slot.MarkDirty();
+                    return;
+                }
+
+            }*/
+            
+            
+            
 
             
             //waterportion
