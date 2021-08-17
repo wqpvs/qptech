cb1161eb5170f2c3b7ebfcdf47eb6dc609c4e12f
Quentin Preik <quentin@preik.net>
2021-08-17 09:43:37 -0400

    Slowly fixing Crucible - currently set to produce copper ingots whenever it can for testing.

--- mods-src/qptech/src/Electricity/BEElectricCrucible.cs
+++ mods-src/qptech/src/Electricity/BEElectricCrucible.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime;
 using Vintagestory.API.Common;
 using Vintagestory.API.MathTools;
 using Vintagestory.API.Common;
@@ -10,20 +11,21 @@ using Vintagestory.API.Datastructures;
 using Vintagestory.API.MathTools;
 using Vintagestory.GameContent;
 using Vintagestory.API.Client;
+using Vintagestory.API.Server;
 using Newtonsoft.Json;
 
 namespace qptech.src
 {
     class BEElectricCrucible : BEElectric, IConduit
     {
-        int tankCapacity = 1000;         //total storage
+        int tankCapacity = 25600;         //total storage
         float maxHeat = 2000;              //how hot can it go
         float minHeat = 20;
         float internalHeat = 20;          //current heat of everything (added items will instantly average their heat)
-        int heatPerTickPerLiter = 500;    //how quickly it can heat it contents
+        int heatPerTickPerLiter = 25000;    //how quickly it can heat it contents
         int heatLossPerTickPerLiter = 10; //how fast to cool contents if not heating
         int fluxPerTick = 1;           //how much power to use
-        
+        int ingotsize = 100;
         public int FreeStorage
         {
             get
@@ -44,7 +46,8 @@ namespace qptech.src
         public bool Full => UsedStorage == FreeStorage;
 
         enStatus status = enStatus.CONSTRUCTION;
-        string currentMetalRecipe = "copper";
+        string currentMetalRecipe = "";
+        int currentOrder = 0;
         Dictionary<string, int> storage; //track internal metals
         public Dictionary<string, int> recipes;
         public enum enStatus { READY,HEATING,PRODUCING,CONSTRUCTION}
@@ -58,18 +61,36 @@ namespace qptech.src
         public override void OnTick(float par)
         {
             base.OnTick(par);
+            //TEMP CODE TO MAKE COPPER
+            if (!(Api is ICoreServerAPI)) { return; }
+            if (storage!=null&& storage.ContainsKey("copper") && storage["copper"] >= 100 && currentOrder == 0)
+            {
+                currentOrder = storage["copper"] / 100;
+                currentMetalRecipe = "copper";
+            }
             if (status == enStatus.CONSTRUCTION) { CheckConstruction();return; }
+            
             CheckInventories();
             DoStorageHeat();
+            SetStatus();
             if (status == enStatus.PRODUCING) { DoProduction(); }
+            MarkDirty(true);
+        }
+
+        void SetStatus()
+        {
+            if (internalHeat < maxHeat) { status = enStatus.HEATING; return; }
+            if (currentOrder > 0 && currentMetalRecipe != "") { status = enStatus.PRODUCING; return; }
+            status = enStatus.READY;
         }
         void CheckConstruction()
         {
-            //TODO Check multiblock structre and do setups as necessary
+           
         }
+        
         void CheckInventories()
         {
-            if (Api is ICoreClientAPI) { return; }
+            
             if (storage == null) { storage = new Dictionary<string, int>();return; }
             if (Full) { return; }
             
@@ -90,14 +111,15 @@ namespace qptech.src
                 {
                     
                     checkbe.MarkDirty(true);
-                    MarkDirty(true);
+                   // MarkDirty(true);
                 }
                 //if (checkslot.Itemstack.StackSize == 0) { checkslot.Itemstack = new ItemStack(); }
             }
+            
         }
         void DoStorageHeat()
         {
-            if (Api is ICoreClientAPI) { return; }
+            //if (Api is ICoreClientAPI) { return; }
             if (UsedStorage == 0) { return; }
             
             //TODO Heat if powered, or cool storage as necessary
@@ -110,13 +132,77 @@ namespace qptech.src
             }
             internalHeat-=(heatLossPerTickPerLiter / (float)UsedStorage);
             internalHeat = Math.Max(internalHeat, minHeat);
-            MarkDirty(true);
+            
+            //MarkDirty(true);
         }
         void DoProduction()
         {
             //TODO if set to produce, check if anything can be made
             //     - make and distribute it
-            if (status != enStatus.PRODUCING) { return; }
+            if (recipes == null) { return; }
+            if (status != enStatus.PRODUCING) { return; } //should probably pre check this
+            if (currentOrder == 0 || currentMetalRecipe == "") { HaltProduction();return; } //problem with order, or it's done - reset status
+            if (!recipes.ContainsKey(currentMetalRecipe)) { HaltProduction();return; } //recipe doesn't exist - reset status
+            if (recipes[currentMetalRecipe] < 1) { return; } //Can't make any of order right now, do nothing
+            
+                MakeOne(); //We should be able to make part of order, do so
+            
+        }
+        void HaltProduction()
+        {
+            currentMetalRecipe = "";
+            currentOrder = 0;
+            status = enStatus.READY;
+        }
+
+        void MakeOne()
+        {
+            //first check if we have the materials to directly make ingot
+            if (storage.ContainsKey(currentMetalRecipe))
+            {
+                if (storage[currentMetalRecipe] >=ingotsize) //enough material to make an ingot
+                {
+                    AssetLocation al = new AssetLocation("game:ingot-" + currentMetalRecipe);
+                    //TODO Will have to check for ingots in other domains?
+                    Item makeitem=Api.World.GetItem(al);
+                    
+                    //Try to make the item
+                    if (CreateItem(makeitem, 1)) {
+                        currentOrder--;
+                        storage[currentMetalRecipe] =storage[currentMetalRecipe]- ingotsize; //take metals out of storage
+                        FindValidRecipes();
+                        
+                    }
+                    else { HaltProduction(); }
+                    //MarkDirty(true);
+                    return;
+                }
+            }
+
+            //next see if there's an alloy recipe matching what we're trying to make
+            AlloyRecipe ar = Api.World.Alloys.Where(i => Api.World.GetItem(i.Output.Code).LastCodePart() == currentMetalRecipe) as AlloyRecipe;
+            if (ar == null) { HaltProduction(); return; }
+        }
+        /// <summary>
+        /// actually create the ingot or other item, return false if something went wrong
+        /// </summary>
+        /// <param name="makeitem">Item To Make</param>
+        /// <param name="makeqty">Quantity To Make</param>
+        /// <returns>true if everything ok, false if there's an issue</returns>
+        bool CreateItem(Item makeitem,int makeqty)
+        {
+            if (makeitem == null) { return false; }
+            //if (!(Api is ICoreClientAPI))
+            //{
+                DummyInventory di = new DummyInventory(Api, 1);
+                di[0].Itemstack = new ItemStack(makeitem, makeqty);
+                di[0].Itemstack.Collectible.SetTemperature(Api.World,di[0].Itemstack,internalHeat);
+                
+                Vec3d pos = Pos.ToVec3d();
+
+                di.DropAll(pos);
+            //}
+            return true;
         }
 
         public int ReceiveItemOffer(ItemSlot offerslot, BlockFacing onFace)
@@ -135,15 +221,17 @@ namespace qptech.src
             if (status == enStatus.CONSTRUCTION) { return 0; }//under construction can't do anything yet
             if (FreeStorage == 0) { return 0; }
             if (offerslot.Itemstack.Item == null) { return 0; }
-            if (!offerslot.Itemstack.Item.Code.ToString().Contains("ingot") && !offerslot.Itemstack.Item.Code.ToString().Contains("nugget")) { return 0; }
+            if (!offerslot.Itemstack.Item.Code.ToString().Contains("ingot") && !offerslot.Itemstack.Item.Code.ToString().Contains("nugget") && !offerslot.Itemstack.Item.Code.ToString().Contains("bit")) { return 0; }
             //now we know we have nuggets and ingots, find the metal
             //List<AlloyRecipe> alloys = Api.World.Alloys;
-            if (offerslot.Itemstack.Collectible.CombustibleProps == null) { return 0; }
-            AssetLocation inass = offerslot.Itemstack.Collectible.CombustibleProps.SmeltedStack.Code; //heeheehee
-            string inmetal = Api.World.GetItem(inass).LastCodePart();//heeheehee
-
+            string inmetal = Api.World.GetItem(offerslot.Itemstack.Item.Code).LastCodePart();
+            if (offerslot.Itemstack.Collectible.CombustibleProps != null)
+            {
+                AssetLocation inass = offerslot.Itemstack.Collectible.CombustibleProps.SmeltedStack.Code; //heeheehee
+                inmetal = Api.World.GetItem(inass).LastCodePart();//heeheee
+            }
             int multiplier = 5;
-            if (offerslot.Itemstack.Item.Code.ToString().Contains("ingot")) { multiplier = 20; }
+            if (offerslot.Itemstack.Item.Code.ToString().Contains("ingot")) { multiplier = ingotsize; }
             float intemp = offerslot.Itemstack.Collectible.GetTemperature(Api.World, offerslot.Itemstack);
             
             int internalused = offerslot.Itemstack.StackSize * multiplier;
@@ -170,7 +258,17 @@ namespace qptech.src
             {
                 dsc.AppendLine(recipes[metal]+" "+metal+" ingots");
             }
-
+            dsc.AppendLine("------");
+            foreach (string key in storage.Keys)
+            {
+                dsc.AppendLine(storage[key] + " units of " + key);
+            }
+            if (currentOrder > 0)
+            {
+                dsc.AppendLine("------");
+                dsc.AppendLine(status.ToString());
+                dsc.AppendLine("Production order " + currentOrder + " of " + currentMetalRecipe);
+            }
         }
         //TODO: Control UI
 
@@ -183,6 +281,7 @@ namespace qptech.src
             //if (type == null) type = defaultType; // No idea why. Somewhere something has no type. Probably some worldgen ruins
             //capacitor = tree.GetInt("capacitor");
             internalHeat=tree.GetFloat("internalHeat");
+            currentOrder = tree.GetInt("currentOrder", 0);
             currentMetalRecipe = tree.GetString("currentMetalRecipe");
             if (!tree.HasAttribute("storage")) { storage = new Dictionary<string, int>(); return; }
             var asString = tree.GetString("storage");
@@ -197,6 +296,8 @@ namespace qptech.src
                     storage = new Dictionary<string, int>();
                 }
             }
+            string statstring = tree.GetString("status");
+            Enum.TryParse(statstring, out enStatus status);
             FindValidRecipes();
         }
         public override void ToTreeAttributes(ITreeAttribute tree)
@@ -206,9 +307,10 @@ namespace qptech.src
             //tree.SetInt("capacitor", Capacitor);
             tree.SetFloat("internalHeat",internalHeat);
             tree.SetString("currentMetalRecipe", currentMetalRecipe);
+            tree.SetInt("currentOrder", currentOrder);
             var asString = JsonConvert.SerializeObject(storage);
             tree.SetString("storage", asString);
-
+            tree.SetString("status", status.ToString());
         }
         //TODO: Start Production, Output Items, Return to Idle status (locks out UI while producing?)
                 
@@ -222,8 +324,8 @@ namespace qptech.src
             //First just add the basic ingot recipes (need 20 units to make an ingot)
             foreach (string key in storage.Keys)
             {
-                if (storage[key] <20) { continue; }
-                recipes[key] = storage[key] / 20;
+                if (storage[key] <ingotsize) { continue; }
+                recipes[key] = storage[key] / ingotsize;
             }
             foreach (AlloyRecipe ar in Api.World.Alloys)
             {
@@ -233,17 +335,18 @@ namespace qptech.src
 
                     string metal= Api.World.GetItem(i.Code).LastCodePart();
                     if (!storage.ContainsKey(metal)) { hasenoughfor = 0; break; }
-                    if (storage[metal]<i.MaxRatio) { hasenoughfor = 0; break; }
-                    float storageqty = (float)storage[metal];
-                    float enoughthisingredient= (storageqty *i.MaxRatio);
+                    
+                    int enoughthisingredient= storage[metal]/(int)(i.MaxRatio*(float)ingotsize);
+                    if (enoughthisingredient < 1) { hasenoughfor = 0;break; }
                     if (hasenoughfor == 0) { hasenoughfor = enoughthisingredient; }
-                    else { hasenoughfor+=enoughthisingredient; }
+                    hasenoughfor = Math.Min(hasenoughfor, enoughthisingredient);
                 }
-                hasenoughfor = hasenoughfor / 20;
+                
                 if (hasenoughfor < 1) { continue; }
                 string outmetal = Api.World.GetItem(ar.Output.Code).LastCodePart();
                 recipes[outmetal] = (int)hasenoughfor;
-
+                //TEMP CODE TO SET PRODUCTION:
+                
             }
         }
     }
