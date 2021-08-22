3d3ec410389f3e37b4dc54fc5c395d0856807ecd
MisterAndyDandy <andy-18-@hotmail.com>
2021-08-23 00:06:04 +0100

    Added Quest Book
    Added Basics - Src, Modinfo, Assets, QuestLoader

---
+++ mods-src/questbook/src/QuestLoader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vintagestory.API.Common;
+
+namespace questbook
+{
+    class QuestBookLoader : ModSystem
+    {
+
+        public override void Start(ICoreAPI api)
+        {
+            base.Start(api);
+        }
+    }
+}
