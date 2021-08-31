acf02d9961559aba38870491cf5551c54e347326
Quentin Preik <quentin@preik.net>
2021-08-31 08:35:33 -0400

    Cleaned up some crucible gui code, still hacking away lol

--- mods-src/qptech/src/gui/GUIElectricCrucible.cs
+++ mods-src/qptech/src/gui/GUIElectricCrucible.cs
@@ -11,18 +11,10 @@ namespace qptech.src
     class GUIElectricCrucible : GuiDialogBlockEntity
     {
         ICoreClientAPI api;
-        double sectionwidth = 280;
-        double sectionheight = 1000;
+
         double sectionx = 0;
         double sectiony = 40;
-        double buttonx;
-        double buttony;
-        double buttonwidth = 200;
-        double buttonheight = 35;
-        double buttonpad = 5;
-        string selection = "";
-        bool isopen = false;
-        //public Vintagestory.API.Common.Action<bool> idkaction ;
+        
         BEElectricCrucible mycrucible;
 
      
@@ -38,12 +30,9 @@ namespace qptech.src
             string guicomponame = mycrucible.Pos.ToString() + "Electric Crucible";
             SingleComposer = capi.Gui.CreateCompo(guicomponame, dialogBounds);
             SetupPowerScreen();
-            //Below is the old gui code
-            //SetupStatusScreen();
-            //SetupInventoryScreen();
-            //SetupProductionScreen();
+
             SingleComposer.Compose();
-            isopen = true;
+            
         }
         public void SetupPowerScreen()
         {
@@ -56,7 +45,7 @@ namespace qptech.src
             //x try and add a switch
             SingleComposer.AddInteractiveElement(new GuiElementSwitch(api, TogglePower, screenBounds, 68, 0));
             activepaneltexture = "electrical panel.png";
-            SingleComposer.AddDynamicCustomDraw(dialogBounds, testdraw, "testbar");
+            SingleComposer.AddDynamicCustomDraw(dialogBounds, Testdraw, "testbar");
             double switchx=5;
             double switchy = 177;
             string statustext = "";
@@ -129,200 +118,41 @@ namespace qptech.src
                 screenxoffest = 0;
                 double screenyoffset = 256;
                 dialogBounds = ElementBounds.Fixed(sectionx + screenxoffest, sectiony+screenyoffset, 512, 256);
-                gdt = new GEDrawTexture(capi, dialogBounds, "blankpanel.png");
-                SingleComposer.AddDynamicCustomDraw(dialogBounds, gdt.OnDraw);
+                //gdt = new GEDrawTexture(capi, dialogBounds, "blankpanel.png");
+                //SingleComposer.AddDynamicCustomDraw(dialogBounds, gdt.OnDraw);
+                double buttonstartx = 20;
+                double buttonstarty = 266;
+                double buttonwidth = 216;
+                double buttonheight = 16;
+                string buttontext = "SINGLE MODE";
+                dialogBounds = ElementBounds.Fixed(sectionx+buttonstartx, sectiony+buttonstarty, buttonwidth, buttonheight);
+                if (mycrucible.Mode == enProductionMode.REPEAT) { buttontext = "REPEAT MODE"; }
+                //TODO why don't buttons render - possibly we can just cheat here and draw our own buttons?
+                SingleComposer.AddGrayBG(dialogBounds);
+                SingleComposer.AddButton(buttontext, onToggleMode, dialogBounds);
             }
 
         }
-        public void SetupStatusScreen()
-        {
-            //ElementBounds dialogBounds = ElementStdBounds.AutosizedMainDialog.WithAlignment(EnumDialogArea.CenterMiddle);
-            ElementBounds dialogBounds= ElementBounds.Fixed(sectionx, sectiony, 1920-40, sectionheight);
-
-
-            ElementBounds textBounds = ElementBounds.Fixed(sectionx, sectiony, sectionwidth, sectionheight);
-
-            // Background boundaries. Again, just make it fit it's child elements, then add the text as a child element
-            //ElementBounds bgBounds = ElementBounds.Fill.WithFixedPadding(GuiStyle.ElementToDialogPadding);
-            ElementBounds bgBounds = ElementBounds.Fill.WithFixedPadding(buttonpad);
-            
-            // bgBounds.BothSizing = ElementSizing.FitToChildren;
-            //bgBounds.WithChildren(textBounds);
-            
-            string statustext = "";
-            statustext += "<font size=\"24\" align=\"center\" >";
-            statustext += "CRUCIBLE STATUS\n\n";
-            if (!mycrucible.IsOn)
-            {
-                statustext += "<font color=#ffff00>POWER OFF</font>";
-            }
-            else
-            {
-                statustext += "Power\n" + (int)(mycrucible.CapacitorPercentage * 100) + "%";
-            }
-            statustext += "\n(flux required: " + mycrucible.FluxPerTick + " flux)";
-            statustext += "\n\nTemperature\n" + (int)(mycrucible.internalTempPercent * 100) + "%";
-            statustext += "\n\nStorage\n" + mycrucible.UsedStorage + "/" + mycrucible.TotalStorage + "\nUnits";
-            statustext += "</font>";
-            buttonx = (sectionwidth / 2)-buttonwidth/2;
-            buttony = sectionheight-50;
-            ElementBounds buttonbounds = ElementBounds.Fixed(buttonx, buttony, buttonwidth, buttonheight);
-            SingleComposer 
-                .AddImageBG(bgBounds, "gui/backgrounds/mainmenu2.png",0)
-                .AddRichtext(statustext, CairoFont.WhiteDetailText(), textBounds)
-                .AddButton("EXIT",OnCloseButton,buttonbounds,EnumButtonStyle.Normal,EnumTextOrientation.Center)
-            ;
-            if (mycrucible.Status == BEElectricCrucible.enStatus.PRODUCING)
-            {
-                buttony -= buttonheight+buttonpad;
-                buttonbounds = ElementBounds.Fixed(buttonx, buttony, buttonwidth, buttonheight);
-                SingleComposer.AddButton("HALT",onHaltProduction, buttonbounds, EnumButtonStyle.Normal, EnumTextOrientation.Center);
-            }
-            string text = "TURN OFF";
-            if (!mycrucible.IsOn) { text = "TURN ON"; }
-            buttony -= buttonheight+buttonpad;
-            buttonbounds = ElementBounds.Fixed(buttonx, buttony, buttonwidth, buttonheight);
-            SingleComposer.AddButton(text, onTogglePower, buttonbounds, EnumButtonStyle.Normal, EnumTextOrientation.Center);
-            
-        }
-
-        public void SetupInventoryScreen()
-        {
-
-            //FIRST ADD TITLE BAR
-            double inventorywidth = sectionwidth * 2;
-            double column1start = sectionx + sectionwidth + 50;
-            double columnwidth = sectionwidth / 2;
-            
-            double columnpad = 25;
-            double titleheight = 50;
-            ElementBounds textBounds = ElementBounds.Fixed(column1start, sectiony, (inventorywidth - 15) / 2, titleheight);
-            string text = "<font size=\"24\" align=\"left\" >";
-            text += "INVENTORY(UNITS)\n\n";
-            text += "</font>";
-            
-
-            //IF INVENTORY IS EMPTY, SHOW MESSAGE, DONE
-
-            if (mycrucible.Storage == null || mycrucible.Storage.Count == 0)
-            {
-                text += "\n\n\nEMPTY";
-                SingleComposer.AddRichtext(text, CairoFont.WhiteDetailText(), textBounds);
-            }
-
-            //OTHERWISE FIRST DRAW METALS IN FIRST COLUMN
-
-            else
-            {
-                SingleComposer.AddRichtext(text, CairoFont.WhiteDetailText(), textBounds); //complete adding the title bar
-                textBounds = ElementBounds.Fixed(column1start, sectiony+titleheight, (inventorywidth - 15) / 2, sectionheight- titleheight);
-                text = "<font size=\"24\" align=\"left\" >";
-                foreach (string key in mycrucible.Storage.Keys)
-                {
-                    text += key.ToUpper() + "\n";
-                }
-                text += "</font>";
-                SingleComposer.AddRichtext(text, CairoFont.WhiteDetailText(), textBounds);
-                //THEN DRAW METAL QUANTITIES IN SECOND COLUMN
-                
-                textBounds = ElementBounds.Fixed(column1start+columnpad+ columnwidth, sectiony + titleheight, (inventorywidth - 15) / 2, sectionheight - titleheight);
-                text = "<font size=\"24\" align=\"right\" >";
-                foreach (string key in mycrucible.Storage.Keys)
-                {
-                    text += mycrucible.Storage[key] + "\n";
-                }
-                text += "</font>";
-                SingleComposer.AddRichtext(text, CairoFont.WhiteDetailText(), textBounds);
-            }
-
-
-
-        }
-        public void SetupProductionScreen()
-        {
-            if (mycrucible.Recipes == null || mycrucible.Recipes.Count == 0) { return; }
-            //FIRST ADD TITLE BAR
-            double inventorywidth = sectionwidth * 2;
-            double column1start = sectionx + sectionwidth*3 + 100;
-            double columnwidth = sectionwidth / 2;
-            double columnpad = 25;
-            double titleheight = 50;
-            int lineheight = 50;
-            ElementBounds textBounds = ElementBounds.Fixed(column1start, sectiony, columnwidth*2, titleheight);
-            string text = "<font size=\"24\" align=\"left\" >";
-            text += "RECIPES(INGOTS)";
-            text += "</font>";
-            SingleComposer.AddRichtext(text, CairoFont.WhiteDetailText(), textBounds); //complete adding the title bar
-
-            //FIRST COLUMN
-            double yorigin = sectiony + titleheight * 1.25;
-            double currentcolumnstart = column1start;
-            double column1width = columnwidth * 2;
-            double ytrack = yorigin;
-            double textoffset = -11;
-            double buttonheight = lineheight - 10;
-            foreach (string key in mycrucible.Recipes.Keys)
-            {
-                textBounds = ElementBounds.Fixed(currentcolumnstart, ytrack-textoffset, column1width, lineheight);
-                text = "<font size=\"24\" align=\"left\" >"+key.ToUpper() + "</font>";
-                SingleComposer.AddRichtext(text, CairoFont.WhiteDetailText(), textBounds);
-                ytrack += lineheight;
-            }
        
-            //THEN DRAW METAL QUANTITIES IN SECOND COLUMN
-            ytrack = yorigin;
-            currentcolumnstart += columnpad + column1width;
-            foreach (string key in mycrucible.Recipes.Keys)
-            {      
-                textBounds = ElementBounds.Fixed(currentcolumnstart, ytrack-textoffset, columnwidth, lineheight);
-                text = "<font size=\"24\" align=\"right\">"+mycrucible.Recipes[key] + "</font>";
-                SingleComposer.AddRichtext(text, CairoFont.WhiteDetailText(), textBounds);
-                ytrack += lineheight;
-            }
-
-            // DRAW MAKE BUTTONS
-            currentcolumnstart += columnpad + columnwidth;
-            ytrack = yorigin;
-            foreach (string key in mycrucible.recipes.Keys)
-            {
-                textBounds = ElementBounds.Fixed(currentcolumnstart, ytrack, columnwidth, buttonheight);
-                SingleComposer.AddButton("MAKE", ()=>onMetalSelect(key,1), textBounds, EnumButtonStyle.Normal, EnumTextOrientation.Center);
-                ytrack += lineheight;
-            }
-
-            
-            
-        }
-        
-
         private void powerswitch(Context ctx, ImageSurface surface, ElementBounds currentBounds)
         {
             ctx.Rectangle(0, 0, currentBounds.InnerWidth, currentBounds.InnerHeight);
-            
             CompositeTexture tex = new CompositeTexture(new AssetLocation("game:block/redhandle.png"));
             Matrix m = ctx.Matrix;
-            
             m.Scale(GuiElement.scaled(GEDrawTexture.scalefactor), GuiElement.scaled(GEDrawTexture.scalefactor));
-            
             ctx.Matrix = m;
-            
-
             AssetLocation loc = tex.Base.Clone().WithPathAppendixOnce(".png");
-            //ImageSurface isurf=new ImageSurface("C:\\Users\\quent\\source\\repos\\wqpvs\\qptech\\mods\\qptech\\assets\\game\\textures\\block\\panel elements.png");
-            //ctx.SetSourceSurface(isurf,0,0);
             GuiElement.fillWithPattern(capi, ctx, loc.Path, true, false);
-            
             ctx.Restore();
             ctx.Save();
         }
 
         string activepaneltexture = "electrical panel.png";
 
-        private void testdraw(Context ctx, ImageSurface surface, ElementBounds currentBounds)
+        private void Testdraw(Context ctx, ImageSurface surface, ElementBounds currentBounds)
         {
             
             ctx.Rectangle(0, 0, currentBounds.InnerWidth, currentBounds.InnerHeight);
-            //CompositeTexture tex = liquidSlot.Itemstack.Collectible.Attributes?["waterTightContainerProps"]?["texture"]?.AsObject<CompositeTexture>(null, liquidSlot.Itemstack.Collectible.Code.Domain);
             CompositeTexture tex = new CompositeTexture(new AssetLocation("game:block/"+activepaneltexture));
             
             if (tex != null)
@@ -331,25 +161,12 @@ namespace qptech.src
                 Matrix m = ctx.Matrix;
                 m.Scale(GuiElement.scaled(GEDrawTexture.scalefactor), GuiElement.scaled(GEDrawTexture.scalefactor));
                 ctx.Matrix = m;
-                
-
                 AssetLocation loc = tex.Base.Clone().WithPathAppendixOnce(".png");
-
                 GuiElement.fillWithPattern(capi, ctx, loc.Path, true, false);
-
                 ctx.Restore();
             }
         }
-        private bool onMetalSelect(string key, int qty)
-        {
-            mycrucible.SetOrder(key, qty, true);
-            SetupStatusScreen();
-            SetupInventoryScreen();
-            SetupProductionScreen();
-            SingleComposer.Compose();
-            return true;
-           
-        }
+        
         public override bool TryOpen()
         {
 
@@ -372,21 +189,21 @@ namespace qptech.src
         public bool onTogglePower()
         {
             mycrucible.ButtonTogglePower();
-            SetupStatusScreen();
+            
             SingleComposer.Compose();
             return true;
         }
         public bool onToggleMode()
         {
             mycrucible.ButtonToggleMode();
-            SetupStatusScreen();
+            
             SingleComposer.Compose();
             return true;
         }
         public void TogglePower(bool onoff)
         {
             mycrucible.ButtonTogglePower();
-            SetupStatusScreen();
+            
             SingleComposer.Compose();
         }
         public override bool TryClose()
