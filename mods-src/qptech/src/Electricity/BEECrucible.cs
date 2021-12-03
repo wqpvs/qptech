fbb8e9a169ea5ce4ba846514445ee66e21c62947
Quentin Preik <quentin@preik.net>
2021-12-03 10:36:48 -0500

    Added wiremill model

---
+++ mods-src/qptech/src/Electricity/BEECrucible.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using qptech.src.networks;
+using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
+
+namespace qptech.src
+{
+    /// <summary>
+    /// Simple electric crucible - gets nuggets, heats, and exports ingots
+    /// BEElectric Crucible will be the larger version with complex interface etc
+    /// </summary>
+    class BEECrucible:BEEBaseDevice
+    {
+        //check input chest for nuggets
+        //if found (and enough there) look up the combustiblePropsByType.meltingPoint
+        //check if enough heat supplied (firepit, or Industiral Process?)
+        //if there is enough then take the nuggets begin processing
+        //output ingot when done, heated appropriately
+
+    }
+}
