8a65dd6a1aaa5cfbc0abbe0d14f8f0421c88f0e4
Quentin Preik <quentin@preik.net>
2022-01-09 09:11:40 -0500

    Added sounds and particles to mining drill - added fuel use but need to figure out some stuff.

--- mods-src/qptech/src/misc/ItemMiningDrill.cs
+++ mods-src/qptech/src/misc/ItemMiningDrill.cs
@@ -10,6 +10,7 @@ using Vintagestory.API.Config;
 using Vintagestory.API.Datastructures;
 using Vintagestory.API.MathTools;
 using Vintagestory.API.Util;
+using Vintagestory.API.Client;
 
 namespace qptech.src.misc
 {
@@ -18,11 +19,21 @@ namespace qptech.src.misc
         float nextactionat = 0;
         bool soundplayed = false;
         float actionspeed = 1f;
+        float soundlevel = 1f;
+        float SoundLevel => soundlevel;
+        bool alreadyPlayedSound = false;
+        bool loopsound = true;
+        int soundoffdelaycounter = 0;
+        float fuel = 100;
+        public static SimpleParticleProperties myParticles = new SimpleParticleProperties(1, 1, ColorUtil.ColorFromRgba(0, 0, 0,75), new Vec3d(), new Vec3d(), new Vec3f(), new Vec3f());
+        ILoadedSound ambientSound;
+        string runsound = "sounds/drillloop";
+        ICoreClientAPI capi;
         public override void OnHeldInteractStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handling)
         {
             //base.OnHeldInteractStart(slot, byEntity, blockSel, entitySel, firstEvent, ref handling);
             nextactionat = actionspeed;
-
+           
 
             handling = EnumHandHandling.Handled;
         }
@@ -35,12 +46,29 @@ namespace qptech.src.misc
             // - break back block
             // - reset
             if (blockSel == null) { return false; }
+            if (fuel <= 0) { return false; }
             //if (!BlockFacing.HORIZONTALS.Contains(blockSel.Face)) { return false; } //not pointed at a block ahead, cancel
             if (secondsUsed > 0.25f && !soundplayed)
             {
                 //api.World.PlaySoundAt(new AssetLocation("sounds/quarrytemp"), blockSel.Position.X, blockSel.Position.Y, blockSel.Position.Z, null, false, 8, 1);
                 soundplayed = true;
             }
+            myParticles.MinPos = byEntity.Pos.XYZ.Add(-0.25, 1, 0).Ahead(1f, byEntity.Pos.Pitch, byEntity.Pos.Yaw);
+            myParticles.AddPos = new Vec3d(0.5, 0.5, 0.5);
+            myParticles.MinVelocity = new Vec3f(0, -0.1f, 0);
+            myParticles.AddVelocity = new Vec3f(0, 0.5f, 0);
+            myParticles.LifeLength = 1F;
+            myParticles.addLifeLength = 0.5f;
+            myParticles.MinQuantity = 3;
+            myParticles.AddQuantity = 10;
+            myParticles.GravityEffect = 0F;
+            //myParticles.MinSize = 0.1F;
+            //myParticles.MaxSize = 1.0F;
+            myParticles.SizeEvolve = new EvolvingNatFloat(EnumTransformFunction.LINEAR, 2);
+            myParticles.ParticleModel = EnumParticleModel.Quad;
+            byEntity.World.SpawnParticles(myParticles);
+            myParticles.OpacityEvolve = new EvolvingNatFloat(EnumTransformFunction.LINEAR, -255);
+            ToggleAmbientSounds(true, blockSel.Position);
             if (secondsUsed > nextactionat)
             {
 
@@ -89,11 +117,13 @@ namespace qptech.src.misc
                     if (tb == null) { continue; }
                     if (tb.MatterState != EnumMatterState.Solid) { continue; }
                     if (tb.RequiredMiningTier > 5) { continue; }
+                    if (!api.World.Claims.TryAccess(p, bp, EnumBlockAccessFlags.BuildOrBreak)) { continue; }
                     tb.OnBlockBroken(api.World, bp, p, 1);
                 
                     
                 }
                 nextactionat += actionspeed;
+                fuel--;
             }
             return true;
         }
@@ -102,9 +132,66 @@ namespace qptech.src.misc
         {
             soundplayed = false;
             nextactionat = 0;
+            CleanSound();
             base.OnHeldInteractStop(secondsUsed, slot, byEntity, blockSel, entitySel);
         }
+        public override void OnHeldDropped(IWorldAccessor world, IPlayer byPlayer, ItemSlot slot, int quantity, ref EnumHandling handling)
+        {
+            base.OnHeldDropped(world, byPlayer, slot, quantity, ref handling);
+            CleanSound();
+        }
 
+        public override void OnUnloaded(ICoreAPI api)
+        {
+            base.OnUnloaded(api);
+            CleanSound();
+        }
+
+        void CleanSound()
+        {
+            ambientSound?.Stop();
+            ambientSound?.Dispose();
+            ambientSound = null;
+        }
+
+        public void ToggleAmbientSounds(bool on, BlockPos Pos)
+        {
+            if (api.Side != EnumAppSide.Client) return;
+            if (runsound == "" || SoundLevel == 0) { return; }
+            if (on)
+            {
+
+                if (ambientSound == null || !ambientSound.IsPlaying && (!alreadyPlayedSound || loopsound))
+                {
+                    ambientSound = ((IClientWorldAccessor)api.World).LoadSound(new SoundParams()
+                    {
+                        Location = new AssetLocation(runsound),
+                        ShouldLoop = loopsound,
+                        Position = Pos.ToVec3f().Add(0.5f, 0.25f, 0.5f),
+                        DisposeOnFinish = false,
+                        Volume = SoundLevel,
+                        Range = 10
+                    });
+                    soundoffdelaycounter = 0;
+                    ambientSound.Start();
+                    alreadyPlayedSound = true;
+                }
+            }
+            else if (loopsound && soundoffdelaycounter < 10)
+            {
+                soundoffdelaycounter++;
+            }
+            else
+            {
+                ambientSound?.Stop();
+                ambientSound?.Dispose();
+                ambientSound = null;
+                alreadyPlayedSound = false;
+            }
+
+        }
+        
     }
+
 }
 
