ffb83a74f27b6f763b589cc9a328713ff011cb9e
Quentin Preik <quentin@preik.net>
2022-01-21 08:55:46 -0500

    more conveyor coding

--- mods-src/qptech/src/itemtransport/Conveyor.cs
+++ mods-src/qptech/src/itemtransport/Conveyor.cs
@@ -23,13 +23,13 @@ namespace qptech.src.itemtransport
         ItemStack itemstack;
         public ItemStack ItemStack => itemstack;
 
-        float progress;
+        float progress=0;
         public float Progress => progress;
 
-        BlockFacing inputface;
+        BlockFacing inputface=BlockFacing.WEST;
         public BlockFacing TransporterInputFace => inputface;
 
-        BlockFacing outputface;
+        BlockFacing outputface=BlockFacing.EAST;
         public BlockFacing TransporterOutputFace => outputface;
 
         public BlockPos TransporterPos => Pos;
@@ -39,6 +39,7 @@ namespace qptech.src.itemtransport
         public override void Initialize(ICoreAPI api)
         {
             base.Initialize(api);
+            if (api is ICoreServerAPI) { RegisterGameTickListener(OnServerTick, 100); }
         }
 
         public bool ConnectSource(IItemTransporter newsource)
@@ -53,6 +54,57 @@ namespace qptech.src.itemtransport
             if (ItemStack == null) { itemstack = incomingstack; MarkDirty(true); return true; }
             return false;
         }
+        public void OnServerTick(float dt)
+        {
+            VerifyConnections();
+            HandleStack();
+        }
+
+        protected virtual void VerifyConnections()
+        {
+            //if it has connections, make sure they're still there
+            //if there aren't any connections, check and see if a destination can be set and connect
+        }
+
+        protected virtual void HandleStack()
+        {
+            //if there is a destination and an item stack, handle movement, trigger rendering if necessary
+            //if movement is complete handle transfer to destination
+        }
+
+        public override void ToTreeAttributes(ITreeAttribute tree)
+        {
+            base.ToTreeAttributes(tree);
+            tree.SetFloat("progress", progress);
+            tree.SetItemstack("itemstack", itemstack);
+            if (Destination == null) { tree.SetBlockPos("destpos", Pos); }
+            else { tree.SetBlockPos("destpos", Destination.TransporterPos); }
+            if (Source == null) { tree.SetBlockPos("sourcepos", Pos); }
+            else { tree.SetBlockPos("sourcepos", source.TransporterPos); }
+        }
+
+        public override void FromTreeAttributes(ITreeAttribute tree, IWorldAccessor worldAccessForResolve)
+        {
+            base.FromTreeAttributes(tree, worldAccessForResolve);
+            progress = tree.GetFloat("progress");
+            itemstack = tree.GetItemstack("itemstack");
+            if (itemstack != null)
+            {
+                itemstack.ResolveBlockOrItem(worldAccessForResolve);
+            }
+            BlockPos sourcepos = tree.GetBlockPos("sourcepos");
+            if (sourcepos==null || sourcepos == Pos) { source = null; }
+            else
+            {
+                source = Api.World.BlockAccessor.GetBlockEntity(sourcepos) as IItemTransporter;
+            }
+            BlockPos destpos = tree.GetBlockPos("destpos");
+            if (destpos == null || destpos == Pos) { destination = null; }
+            else
+            {
+                destination = Api.World.BlockAccessor.GetBlockEntity(destpos) as IItemTransporter;
+            }
+        }
 
         public override void GetBlockInfo(IPlayer forPlayer, StringBuilder dsc)
         {
@@ -62,5 +114,6 @@ namespace qptech.src.itemtransport
             if (destination != null) { dsc.AppendLine("To " + destination.TransporterPos.ToString()); }
 
         }
+
     }
 }
