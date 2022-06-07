ba31496a08abeb3d9103bbb5b0ba41b1a5ee2e7d
Quentin Preik <quentin@preik.net>
2022-06-07 07:44:14 -0400

    Sound,Graphics,Columns working for Temporal Pocket - still need to handle stuff like preventing inventory insertion.

--- mods-src/qptech/src/misc/BETemporalPocket.cs
+++ mods-src/qptech/src/misc/BETemporalPocket.cs
@@ -16,16 +16,21 @@ using Vintagestory.API.Config;
 using Vintagestory.API.Util;
 using Vintagestory.ServerMods;
 using qptech.src.extensions;
-
+using System.Text.RegularExpressions;
 
 namespace qptech.src
 {
-    class BETemporalPocket:BlockEntityGenericContainer
+    class BETemporalPocket:BlockEntityOpenableContainer
     {
         string accessing = "";
         public bool Busy => (accessing!="");
         static List<string> openinventories;
-        
+        public int quantitySlots = 64;
+        public int quantityColumns = 8;
+        public string inventoryClassName = "temporalpocket";
+        public string dialogTitleLangCode = "Temporal Pocket";
+        public bool retrieveOnly = false;
+
         public static List<string> OpenInventories
         {
             get
@@ -35,11 +40,60 @@ namespace qptech.src
             }
         }
         public override string InventoryClassName => "Temporal Pocket";
-        protected override void OnInvOpened(IPlayer player)
+        internal InventoryGeneric inventory;
+
+        public override InventoryBase Inventory
+        {
+            get { return inventory; }
+        }
+
+        public override AssetLocation OpenSound { get; set; } = new AssetLocation("machines:sounds/block/hmmm");
+        public override AssetLocation CloseSound { get; set; } = new AssetLocation("machines:sounds/block/hmmm");
+
+        public override void Initialize(ICoreAPI api)
+        {
+            // Newly placed 
+            if (inventory == null)
+            {
+                InitInventory(Block);
+            }
+
+            base.Initialize(api);
+        }
+        
+        
+        private void InitInventory(Block Block)
+        {
+            if (Block?.Attributes != null)
+            {
+                inventoryClassName = Block.Attributes["inventoryClassName"].AsString(inventoryClassName);
+                dialogTitleLangCode = Block.Attributes["dialogTitleLangCode"].AsString(dialogTitleLangCode);
+                quantitySlots = Block.Attributes["quantitySlots"].AsInt(quantitySlots);
+                retrieveOnly = Block.Attributes["retrieveOnly"].AsBool(false);
+            }
+
+            inventory = new InventoryGeneric(quantitySlots, null, null, null);
+
+            if (Block.Attributes?["spoilSpeedMulByFoodCat"].Exists == true)
+            {
+                inventory.PerishableFactorByFoodCategory = Block.Attributes["spoilSpeedMulByFoodCat"].AsObject<Dictionary<EnumFoodCategory, float>>();
+            }
+
+            if (Block.Attributes?["transitionSpeedMulByType"].Exists == true)
+            {
+                inventory.TransitionableSpeedMulByType = Block.Attributes["transitionSpeedMulByType"].AsObject<Dictionary<EnumTransitionType, float>>();
+            }
+
+            inventory.OnInventoryClosed += OnInvClosed;
+            inventory.OnInventoryOpened += OnInvOpened;
+            inventory.SlotModified += OnSlotModifid;
+        }
+        protected  void OnInvOpened(IPlayer player)
         {
             //if (simpleinventory.openinventories == null) { simpleinventory.openinventories = new List<string>(); }
-           // if (simpleinventory.openinventories.Contains(player.PlayerUID)) { return; }
-           if (OpenInventories.Contains(player.PlayerUID)) { return; }
+            // if (simpleinventory.openinventories.Contains(player.PlayerUID)) { return; }
+            inventory.PutLocked = retrieveOnly && player.WorldData.CurrentGameMode != EnumGameMode.Creative;
+            if (OpenInventories.Contains(player.PlayerUID)) { return; }
             OpenInventories.Add(player.PlayerUID);
             if (accessing != "") { return; }
             accessing = player.PlayerUID;
@@ -47,13 +101,15 @@ namespace qptech.src
             if (Api is ICoreClientAPI) { return; }
             TryLoadInventory(player);
             
-            base.OnInvOpened(player);
+            
         }
 
-        protected override void OnInvClosed(IPlayer player)
+        protected  void OnInvClosed(IPlayer player)
         {
+            inventory.PutLocked = retrieveOnly;
+            invDialog?.Dispose();
+            invDialog = null;
 
-            base.OnInvClosed(player);
             accessing = "";
             OpenInventories.Remove(player.PlayerUID);
             if (Api is ICoreClientAPI) { return; }
@@ -64,9 +120,17 @@ namespace qptech.src
             
             
         }
+        private void OnSlotModifid(int slot)
+        {
+            Api.World.BlockAccessor.GetChunkAtBlockPos(Pos)?.MarkModified();
+        }
         public virtual string GetChestFilename(IPlayer player)
         {
-            return player.PlayerUID + "tchest.json";
+            return RemoveSpecialCharacters(player.PlayerUID) + "tchest.json";
+        }
+        public static string RemoveSpecialCharacters(string str)
+        {
+            return Regex.Replace(str, "[^a-zA-Z0-9_.]+", "", RegexOptions.Compiled);
         }
         void TryLoadInventory(IPlayer player)
         {
@@ -137,7 +201,65 @@ namespace qptech.src
             
             this.Inventory.DiscardAll();
         }
+
+        public override bool OnPlayerRightClick(IPlayer byPlayer, BlockSelection blockSel)
+        {
+            if (Api.World is IServerWorldAccessor)
+            {
+                byte[] data;
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    BinaryWriter writer = new BinaryWriter(ms);
+                    writer.Write("BlockEntityInventory");
+                    writer.Write(Lang.Get(dialogTitleLangCode));
+                    writer.Write((byte)8);
+                    TreeAttribute tree = new TreeAttribute();
+                    inventory.ToTreeAttributes(tree);
+                    tree.ToBytes(writer);
+                    data = ms.ToArray();
+                }
+
+                ((ICoreServerAPI)Api).Network.SendBlockEntityPacket(
+                    (IServerPlayer)byPlayer,
+                    Pos.X, Pos.Y, Pos.Z,
+                    (int)EnumBlockContainerPacketId.OpenInventory,
+                    data
+                );
+
+                byPlayer.InventoryManager.OpenInventory(inventory);
+            }
+
+            return true;
+        }
+        public override void ToTreeAttributes(ITreeAttribute tree)
+        {
+            base.ToTreeAttributes(tree);
+            
+            if (Block != null) tree.SetInt("forBlockId", Block.BlockId);
+            if (Block != null) tree.SetString("accessing", accessing);
+        }
+        public override void FromTreeAttributes(ITreeAttribute tree, IWorldAccessor worldForResolving)
+        {
+            if (inventory == null)
+            {
+                if (tree.HasAttribute("forBlockId"))
+                {
+                    InitInventory(worldForResolving.GetBlock((ushort)tree.GetInt("forBlockId")));
+                }
+                else
+                {
+                    ITreeAttribute inventroytree = tree.GetTreeAttribute("inventory");
+                    
+
+                    InitInventory(null);
+                }
+            }
+            accessing = tree.GetString("accessing", "");
+
+            base.FromTreeAttributes(tree, worldForResolving);
+        }
     }
 
-    
+
 }
