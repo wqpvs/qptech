c01da6b265eabd867752ff501061f088645b24c9
Quentin Preik <quentin@preik.net>
2022-06-27 11:47:40 -0400

    Wooden rails now craftable (thanks to Xel)

--- mods-src/qptech/src/rails/MinecartEntity.cs
+++ mods-src/qptech/src/rails/MinecartEntity.cs
@@ -550,10 +550,18 @@ namespace qptech.src.rails
                     if (myslot == null || myslot.Empty) { continue; }
                     foreach (ItemSlot slot in outcont.Inventory)
                     {
-                        
+                        if (Api == null || World == null) { break; }
+                        if (slot == null || outcont == null || myslot == null) { continue; }
                         if (!slot.CanHold(myslot)) { continue; }
-                        
-                        int moved =myslot.TryPutInto(Api.World, slot);
+                        int moved = 0;
+                        try
+                        {
+                            moved = myslot.TryPutInto(Api.World, slot);
+                        }
+                        catch
+                        {
+                            break;
+                        }
                         myslot.MarkDirty();
                         slot.MarkDirty();
                         if (moved > 0) {  return false; }
