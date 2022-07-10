a777ff73c2bafb38a4a368ff6a33a4ee05d340ea
Quentin Preik <quentin@preik.net>
2022-07-10 13:04:12 -0400

    Revert "1.17 Pre Release versions - all seems to be good!"
    This reverts commit cac78958a0f0d903380beb33dff4ae62540c99e0.

--- mods-src/qptech/src/misc/ItemJetPack.cs
+++ mods-src/qptech/src/misc/ItemJetPack.cs
@@ -39,7 +39,7 @@ namespace qptech.src.misc
                 
             Vec3d vecthrust = Vec3d.Zero;
             bool hovermode = false;
-            if (byEntity.Pos.Motion.Y < maxSpeed )
+            if (byEntity.Pos.Motion.Y < maxSpeed && byEntity.HeadPitch<-0.05f)
             {
                 byEntity.Pos.Motion.Y = thrust*throttlePercent;
             }
